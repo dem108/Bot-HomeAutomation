b08dfb240869b487123ee5ac6bbb9251d68c0d1d
한석진 <dem108ht@hotmail.com>
2017-09-03 22:53:55 +0900

    Created Initial LUIS model, connected it to LuisBaseDialog. After Welcome message and sample CardAction buttions, route messages to LuisBaseDialog.

---
+++ Bot-HomeAutomation/Dialogs/LuisBaseDialog.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+using System.Configuration;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace Bot_HomeAutomation.Dialogs
+{
+   /*
+ * Data Models used in API / Device Communication 
+ */
+    public enum ElementType
+    {
+        LGT = 0,
+        FAN = 1,
+        CAM = 2,
+        TMP = 3,
+        HMD = 4,
+        ALL = 5
+    }
+    public enum ElementState
+    {
+        OFF = 0,
+        ON = 1,
+        VARIANT = 2,
+    }
+
+    public class DeviceElement
+    {
+        public ElementType ElementType { get; set; }
+        public ElementState ElementState { get; set; }
+        public string Value { get; set; }
+    }
+    public class DeviceElementStatus
+    {
+        public string DeviceId { get; set; }
+        public DeviceElement[] DeviceElements { get; set; }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+
+
+
+    [LuisModel("2b768735-9bd2-4c31-9f89-74945da5ce01", "d44d9ba581e5412abd2352835c2c6385", domain: "southeastasia.api.cognitive.microsoft.com", Staging =false)]
+    [Serializable]
+    public class LuisBaseDialog : LuisDialog<object>
+    {
+        static HttpClient httpclient;
+
+        static void PostAsync(string requestUri, DeviceElementStatus control)
+        {
+            var content = JsonConvert.SerializeObject(control);
+            var response = httpclient
+                .PostAsync(requestUri, new StringContent(content, Encoding.UTF8, "application/json"))
+                .ContinueWith(responseTask =>
+                {
+                    var result = responseTask.Result;
+                    var json = result.Content.ReadAsStringAsync();
+                    Console.WriteLine(json.Result);
+                });
+            response.Wait();
+        }
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Sorry, I did not understand '{result.Query}'. Type 'help' if you need assistance.";
+            
+            await context.PostAsync(message);
+            context.Wait(this.MessageReceived);
+        }
+        
+        [LuisIntent("Help")]
+        public async Task Help(IDialogContext context, LuisResult result)
+        {
+            string message = $"Help: You can do these things...";
+
+            await context.PostAsync(message);
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("Switch.Light.Off")]
+        public async Task SwitchLightOff(IDialogContext context, LuisResult result)
+        {
+            string message = $"SwitchLightOff: Turning off the light...";
+            await context.PostAsync(message);
+
+            httpclient = new HttpClient
+            {
+                BaseAddress = new Uri("http://homeautomationplatform.azurewebsites.net/")
+            };
+            var deviceElementStatus = new DeviceElementStatus
+            {
+                DeviceId = "gbb-kickoff-demo",
+                DeviceElements = new DeviceElement[]{
+                    new DeviceElement()
+                    {
+                        ElementType = ElementType.LGT,
+                        ElementState = ElementState.OFF,
+                        Value = "0.00"
+                    }
+                }
+            };
+
+            PostAsync("api/element/setdevices", deviceElementStatus);
+            
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("Switch.Light.On")]
+        public async Task SwitchLightOn(IDialogContext context, LuisResult result)
+        {
+            string message = $"SwitchLightOn: Turning on the light...";
+            await context.PostAsync(message);
+
+
+            httpclient = new HttpClient
+            {
+                BaseAddress = new Uri("http://homeautomationplatform.azurewebsites.net/")
+            };
+            var deviceElementStatus = new DeviceElementStatus
+            {
+                DeviceId = "gbb-kickoff-demo",
+                DeviceElements = new DeviceElement[]{
+                    new DeviceElement()
+                    {
+                        ElementType = ElementType.LGT,
+                        ElementState = ElementState.ON,
+                        Value = "0.00"
+                    }
+                }
+            };
+
+            PostAsync("api/element/setdevices", deviceElementStatus);
+
+            context.Wait(this.MessageReceived);
+        }
+
+
+
+    }
+
+}
--- Bot-HomeAutomation/Dialogs/RootDialog.cs
+++ Bot-HomeAutomation/Dialogs/RootDialog.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using AdaptiveCards;
+using System.Collections.Generic;
 
 namespace Bot_HomeAutomation.Dialogs
 {
@@ -19,7 +21,7 @@ namespace Bot_HomeAutomation.Dialogs
         {
             var activity = await result as Activity;
 
-            await this.SendWelcomeMessageAsync(context);
+            await this.SendWelcomeMessageAsync(context, activity);
 
             /*
             // calculate something for us to return
@@ -31,15 +33,90 @@ namespace Bot_HomeAutomation.Dialogs
             */
         }
 
-        private async Task SendWelcomeMessageAsync(IDialogContext context)
+        private async Task SendWelcomeMessageAsync(IDialogContext context, Activity activity)
 
         {
 
+            //TODO
+            //move welcome message to before authentication, when you implemented authentication
             await context.PostAsync("Hi, I'm the GBB Home Automation bot. Let's get started.");
 
-            await context.PostAsync("I can do: Lights on, Lights off.");
+            //TODO
+            //show weather at user location using adaptive card
+            //http://adaptivecards.io/visualizer/?card=/samples/cards/Weather%20Compact.json
+            //but will need to get weather info from somewhere. 
+
+            Activity replyToConversation = activity.CreateReply("");
+            replyToConversation.Attachments = new List<Attachment>();
+
+
             
-            context.Call(new EchoDialog(), this.MessageReceivedAsync);
+            /*
+
+
+
+            var card = new AdaptiveCard();
+
+            card.Body.Add(new TextBlock()
+            {
+                Text = "Home Automation Bot",
+                Size = TextSize.Large,
+                Weight = TextWeight.Bolder,
+                Wrap = true
+            });
+            card.Body.Add(new TextBlock()
+            {
+                Text = "I can do things like turning on and off the light or fan, take a picture in the room etc.",
+                IsSubtle = true,
+                Wrap = true
+            });
+            
+            card.Actions.Add(new OpenUrlAction()
+            {
+                Title = "Turn On Light",
+//                Type = "imBack",
+                Value = "Turn on the light"
+            });
+
+            
+            Attachment attachement = new Attachment()
+            {
+                ContentType = AdaptiveCard.ContentType,
+                Content = card
+            };
+            */
+
+            List<CardAction> cardButtons = new List<CardAction>();
+
+
+            cardButtons.Add(new CardAction()
+            {
+                Value = "Turn on the light",
+                Type = "imBack",
+                Title = "Turn On Light"
+            });
+            cardButtons.Add(new CardAction()
+            {
+                Value = "Turn off the light",
+                Type = "imBack",
+                Title = "Turn Off Light"
+            });
+
+
+            var card = new HeroCard()
+            {
+                Title = "Home Automation Bot",
+                Subtitle = "I can do things like turning on and off the light or fan, take a picture in the room etc.",
+                Buttons = cardButtons
+            };
+
+            Attachment attachment = card.ToAttachment();
+
+            replyToConversation.Attachments.Add(attachment);
+
+            await context.PostAsync(replyToConversation);
+
+            context.Call(new LuisBaseDialog(), this.MessageReceivedAsync);
 
         }
     }
